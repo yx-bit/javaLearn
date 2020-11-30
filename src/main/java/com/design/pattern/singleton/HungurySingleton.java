bc4d280dca940d6ead7591e3c327bdcdaefcd3d0
chenyx <202402590@qq.com>
2020-11-30 22:57:18 +0800

    饿汉模式

---
+++ src/main/java/com/design/pattern/singleton/HungurySingleton.java
@@ -0,0 +1,22 @@
+package com.design.pattern.singleton;
+
+public class HungurySingleton {
+    private static HungurySingleton instance = new HungurySingleton();
+
+    private HungurySingleton() {
+    }
+
+    public static HungurySingleton getInstance() {
+        return instance;
+    }
+}
+ class HungurySingletonTest{
+     public static void main(String[] args) {
+         //true  类加载初始化赋值 借助jvm类加载机制，保证实例唯一性
+         //类加载过程
+         //1. 加载二进制数据到内存中，生成对应的Class数据结构
+         //2.链接 a 验证 b 准备（给类的静态成员变量赋默认值） c 解析
+         //3. 初始化 给类的静态变量赋初始值
+         System.out.println(HungurySingleton.getInstance()==HungurySingleton.getInstance());
+     }
+}
