af362b645c316343640889c05283e342cd7afdb8
yx-linux <202402590@qq.com>
2020-12-05 14:06:22 +0800

    简单工厂模式、抽象工厂模式

--- src/main/java/com/design/pattern/factory/FactoryMethod.java
+++ src/main/java/com/design/pattern/factory/FactoryMethod.java
@@ -1,6 +1,6 @@
 package com.design.pattern.factory;
 
-//抽象工厂模式
+//工厂方法模式
 public class FactoryMethod {
     public static void main(String[] args) {
         AbstractProduct abstractProduct = new CreateProductA();
