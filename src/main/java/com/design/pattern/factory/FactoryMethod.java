816bc9169e0a19a9061f8b425f6d091d2a2d87de
yx-linux <202402590@qq.com>
2020-12-05 13:57:25 +0800

    简单工厂模式、抽象工厂模式

--- src/main/java/com/design/pattern/factory/FactoryMethod.java
+++ src/main/java/com/design/pattern/factory/FactoryMethod.java
@@ -1,55 +1,31 @@
 package com.design.pattern.factory;
 
+//抽象工厂模式
 public class FactoryMethod {
     public static void main(String[] args) {
-//        Product product = SimpleFactory.createProduct("0");
-//        product.method();
-        AbstractProduct abstractProduct = new createProductA();
+        AbstractProduct abstractProduct = new CreateProductA();
         Product product = abstractProduct.getProduct();
         product.method();
 
     }
 
-
 }
-//一个接口  定义不变的方法
-interface Product{
-    void method();
-}
-//不同类型实现接口  体现了类的多态性
-class ProductA implements Product{
-    @Override
-    public void method() {
-        System.out.println("ProductA    method1");
-    }
-}
-//提供一个工厂根据不同业务匹配具体实现业务
-class SimpleFactory{
-    public static Product createProduct(String type) {
-        //预处理
-
-        if ("0".equals(type)) {
-            return new ProductA();
-        }else{
-            return null;
-        }
-    }
-}
+//定义一个抽象的模式
 abstract class AbstractProduct {
-    abstract ProductA createProduct();
+    abstract Product createProduct();
     Product getProduct(){
         //预处理
-        ProductA product = createProduct();
+        Product product = createProduct();
         //后处理
         return product;
     }
 
 }
-
-class createProductA extends AbstractProduct {
+//不同类型继承抽象类  体现多态性
+class CreateProductA extends AbstractProduct {
 
     @Override
-    ProductA createProduct() {
+    Product createProduct() {
         return new ProductA();
     }
 }
