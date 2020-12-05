5fe9e40f1c48ed4375e75db4d40a743d573488e7
yx-linux <202402590@qq.com>
2020-12-05 14:09:49 +0800

    添加springboot支持

---
+++ src/main/java/com/design/pattern/DesignPatternApp.java
@@ -0,0 +1,11 @@
+package com.design.pattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternApp {
+    public static void main(String[] args) {
+        SpringApplication.run(DesignPatternApp.class,args);
+    }
+}
