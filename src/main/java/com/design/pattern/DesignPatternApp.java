fe6ce2ab42d0873df2ca6c069f9bb402f78abf99
yx-linux <202402590@qq.com>
2020-12-05 14:20:05 +0800

    If you have database settings to be loaded from a particular profile you may 在启动类的@EnableAutoConfiguration或 @SpringBootApplication中添加exclude = {DataSourceAutoConfiguration.class}

--- src/main/java/com/design/pattern/DesignPatternApp.java
+++ src/main/java/com/design/pattern/DesignPatternApp.java
@@ -2,8 +2,9 @@ package com.design.pattern;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class DesignPatternApp {
     public static void main(String[] args) {
         SpringApplication.run(DesignPatternApp.class,args);
