b27844f782e56707fa6e2688c2d6c1ce39849223
yx-linux <202402590@qq.com>
2020-12-13 13:49:52 +0800

    观察者模式

---
+++ src/main/java/com/design/pattern/observer/Observer.java
@@ -0,0 +1,43 @@
+package com.design.pattern.observer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Observer {
+
+    public static void main(String[] args) {
+        Subject subject = new Subject();
+        Task observer = new Task();
+        subject.addObserver(observer);
+        subject.notifyObserver("xxxxx");
+    }
+}
+
+class Subject {
+    List<ObserverInterface> observerList = new ArrayList<>();
+
+    public void addObserver(ObserverInterface observer) {
+        observerList.add(observer);
+    }
+
+    public void removeObserver(ObserverInterface observer) {
+        observerList.remove(observer);
+    }
+
+    public void notifyObserver(Object object) {
+        for (ObserverInterface observer : observerList) {
+            observer.update(object);
+        }
+    }
+}
+
+interface ObserverInterface {
+    void update(Object object);
+}
+
+class Task implements ObserverInterface {
+    @Override
+    public void update(Object object) {
+        System.out.println("任务进行。。。"+object);
+    }
+}
